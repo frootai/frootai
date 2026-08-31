06be163bcbe81789991c7508f96f1ad223e7f744
Pavleen Bali <pavleenbali@microsoft.com>
2026-08-31 15:20:29 +0200

    chore: move controlled client implementations private

--- vscode-extension/src/data/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./plays";
-export * from "./modules";
-export * from "./tools";
-export * from "./costs";
--- vscode-extension/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./knowledge";
-export * from "./cache";
-export * from "./markdown";
